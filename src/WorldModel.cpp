631a18e99740df1d60af47d55588a50a9b834db1
jshellman <joel@mentics.com>
2018-04-05 08:33:50 -0700

    Some more fixes

--- src/WorldModel.cpp
+++ src/WorldModel.cpp
@@ -6,6 +6,7 @@ namespace MenticsGame {
 
 void WorldModel::createQuip(RealTime at, TrajectoryUniquePtr&& traj)
 {
+	mlog->info("Creating quip");
 	agents.quips.add(Quip<RealTime>(0, std::move(traj), 0, 0, 0, 0),at);
 }
 
